5d640b5744683a4be309737cdb84663076978bc1
erfan <erfan.wtf44@gmail.com>
2024-07-15 14:46:15 -0400

    refactor structure

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = require('./server');
+(async function() {
+  try {
+    await db.authenticate();
+    console.log('DB is connected');
+    await db.sync({ logging: false })
+    console.log('sync succefully');
+    app.listen(8000, () => { console.log('server is running on http://localhost:8000') })
+  } catch (err) {
+    console.error(err)
+  }
+})();
