87f693ecfbf4838b22740853ecb068fa1d7a1594
erfan <erfan.wtf44@gmail.com>
2024-07-28 21:41:28 -0400

    add send email class for send html file to user

---
+++ utils/email.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+class Email {
+  constructor() {
+    this.email = process.env.EMAIL
+    this.password = process.env.PASSWORD
+
+    this.transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: this.email,
+        pass: this.password
+      },
+    });
+  }
+
+
+  async sendMail(to, subject, content) {
+    try {
+      await this.transport.sendMail({
+        to,
+        subject,
+        html: content
+      })
+      return true
+    } catch (error) {
+      return false
+    }
+
+  }
+}
+
+module.exports = Email
