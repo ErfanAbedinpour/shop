fa759f2493bea4cab186760d8f1864d64f0e2303
erfan <erfan.wtf44@gmail.com>
2024-08-08 13:19:37 -0400

    better structure for connect in DB

--- utils/constant.js
+++ utils/constant.js
@@ -1,18 +1,10 @@
 const { Sequelize } = require("sequelize");
+const config = require("../config/db.config.js");
 
-const username = process.env.DBUSERNAME;
-const pass = process.env.DBPASS;
-const host = process.env.DBHOST;
-if (process.env.NODE_ENV === "dev") {
-  process.env.DBNAME = "shop";
-} else if (process.env.NODE_ENV === "test") {
-  process.env.DBNAME = "testDB";
-}
-const name = process.env.DBNAME;
-console.log("DB name is ", name);
-
-exports.db = new Sequelize(name, username, pass, {
-  dialect: "mysql",
-  host: host,
-  logging: false,
+console.log(config);
+exports.db = new Sequelize(config.DB, config.USERNAME, config.PASSWORD, {
+    dialect: config.DIALECT,
+    host: config.HOST,
+    port: config.PORT,
+    logging: false,
 });
