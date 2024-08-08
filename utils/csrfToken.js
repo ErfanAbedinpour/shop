f98b0121a35bc68a7df6bd3f08ae620c08440146
erfan <erfan.wtf44@gmail.com>
2024-08-08 13:20:18 -0400

    remove log

--- utils/csrfToken.js
+++ utils/csrfToken.js
@@ -1,30 +1,29 @@
 const Tokens = require("csrf");
 
 function genCsrfToken(req, res, next) {
-  const tokens = new Tokens();
-  tokens.secret(function (err, secret) {
-    if (err) return next(err);
-    const token = tokens.create(secret);
-    req.session.csrf = {
-      secret,
-      token,
-    };
-    next();
-  });
+    const tokens = new Tokens();
+    tokens.secret(function (err, secret) {
+        if (err) return next(err);
+        const token = tokens.create(secret);
+        req.session.csrf = {
+            secret,
+            token,
+        };
+        next();
+    });
 }
 
 function verifyCsrfToken(req, res, next) {
-  const tokens = new Tokens();
-  const { csrf_token } = req.body;
-  if (!tokens.verify(req.session?.csrf?.secret, csrf_token)) {
-    console.log("faildee");
-    req.flash("errors", [{ msg: "لطفا توکن معتبر وارد کنید" }]);
-    return res.redirect(req.originalUrl);
-  }
-  next();
+    const tokens = new Tokens();
+    const { csrf_token } = req.body;
+    if (!tokens.verify(req.session?.csrf?.secret, csrf_token)) {
+        req.flash("errors", [{ msg: "لطفا توکن معتبر وارد کنید" }]);
+        return res.redirect(req.originalUrl);
+    }
+    next();
 }
 
 module.exports = {
-  genCsrfToken,
-  verifyCsrfToken,
+    genCsrfToken,
+    verifyCsrfToken,
 };
